f0f55a42c538320c6360bbe232bc1da8525d9514
Ariel Paricagua <ArielFParicaguaV@gmail.com>
2023-10-12 08:11:42 -0400

    Creacion de la estrutura y los componentes de la aplicacion

---
+++ app/about/page.jsx
@@ -0,0 +1,22 @@
+
+function AboutPage() {
+  return (
+    <div>
+        <h1>About</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing 
+        elit. Voluptatum ab obcaecati necessitatibus, atque quibusdam 
+        temporibus quo vero illo dolor! Dicta similique aspernatur nobis 
+        nam eveniet praesentium quis eum nesciunt cupiditate magnam! 
+        Cupiditate modi laborum laboriosam adipisci voluptatem consectetur 
+        quae ipsum. Dolorum dolor eum debitis ipsam cum voluptatibus odio 
+        amet quis pariatur ea. Impedit consectetur omnis, nostrum harum 
+        dignissimos repellendus? Saepe soluta odit minus labore reiciendis 
+        vero quos perspiciatis molestiae, ad omnis fugiat laborum architecto, 
+        consequuntur, perferendis ipsam voluptatum itaque rerum. Iure officiis 
+        voluptate dolores ea, natus dolorem? Assumenda similique quos aspernatur 
+        iusto, omnis natus earum? Minus odit eius pariatur iusto.</p>
+    </div>
+  )
+}
+
+export default AboutPage
---
+++ app/services/page.jsx
@@ -0,0 +1,11 @@
+
+function ServicesPage() {
+  return (
+    <div>
+        <h1> Services </h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi enim tempora ut ad quam suscipit vel aliquid qui repellendus expedita eveniet error commodi, incidunt consectetur ea nostrum ipsa reiciendis delectus magnam maxime amet modi repudiandae! Cum fuga mollitia, animi quod modi asperiores nulla autem, veniam repellendus sit ullam officia a?</p>
+    </div>
+  )
+}
+
+export default ServicesPage
